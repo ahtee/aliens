f2b194c99b0465b03c850978876de0023f7cc974
212615385 <benjamin.otte@ge.com>
2019-02-06 13:58:31 -0500

    initial commit

---
+++ src/EmojiColumn.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './EmojiColumn.css';
+import alienIcon from './images/ios-alien.png';
+
+class EmojiColumn extends Component {
+    render() {
+      return (
+        <div className="column emoji">
+            <img src={alienIcon} alt="alien" className="alien"/>
+        </div>
+      );
+    }
+  }
+  
+  export default EmojiColumn;
+  
---
+++ src/StaticColumn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './StaticColumn.css';
+
+class StaticColumn extends Component {
+    render() {
+      return (
+        <div className="column staticGif"></div>
+      );
+    }
+  }
+  
+  export default StaticColumn;
+  
